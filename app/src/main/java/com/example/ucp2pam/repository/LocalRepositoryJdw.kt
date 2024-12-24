48e5f4d17730d1350871f9bc6d3681ebc88c24ef
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-12-24 08:41:38 +0700

    membuat Local Repository Jadwal

--- app/src/main/java/com/example/ucp2pam/repository/LocalRepositoryJdw.kt
+++ app/src/main/java/com/example/ucp2pam/repository/LocalRepositoryJdw.kt
@@ -1,3 +1,31 @@
 package com.example.ucp2pam.repository
 
+import com.example.ucp2pam.data.entity.Jadwal
+import com.example.ucp2pam.data.dao.JadwalDao
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryJdw(
+    private val jadwalDao: JadwalDao
+) : RepositoryJdw {
+
+    override suspend fun insertJdw(jadwal: Jadwal) {
+        jadwalDao.insertJd(jadwal)
+    }
+
+    override fun getAllJdw(): Flow<List<Jadwal>> {
+        return jadwalDao.getAllJd()
+    }
+
+    override fun getJdw(idAntrian: String): Flow<Jadwal> {
+        return jadwalDao.getJd(idAntrian)
+    }
+
+    override suspend fun deleteJdw(jadwal: Jadwal) {
+        jadwalDao.deleteJd(jadwal)
+    }
+
+    override suspend fun updateJdw(jawal: Jadwal) {
+        jadwalDao.updateJd(jawal)
+    }
+}
 

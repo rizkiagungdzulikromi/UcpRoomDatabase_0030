1996d9f0811dd0cbe8afb6b1ff2a38811e15cd0f
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-12-24 08:40:18 +0700

    membuat Repository Dokter

--- app/src/main/java/com/example/ucp2pam/repository/RepositoryDr.kt
+++ app/src/main/java/com/example/ucp2pam/repository/RepositoryDr.kt
@@ -1,2 +1,15 @@
 package com.example.ucp2pam.repository
 
+import com.example.ucp2pam.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryDr {
+
+    suspend fun insertDr(dokter: Dokter)
+
+    // getAllDokter
+    fun getAllDr(): Flow<List<Dokter>>
+
+    // getDokter
+    fun getDr(idDr: String): Flow<Dokter>
+}

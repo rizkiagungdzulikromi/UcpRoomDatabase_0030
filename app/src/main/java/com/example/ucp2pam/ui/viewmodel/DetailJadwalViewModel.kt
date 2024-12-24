9d0e6d6caed8875ef2577668d8e6d723864c9919
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-12-24 09:10:08 +0700

    Membuat Detail jadwal view model

---
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/DetailJadwalViewModel.kt
@@ -0,0 +1,62 @@
+package com.example.ucp2pam.ui.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2pam.data.entity.Jadwal
+import com.example.ucp2pam.repository.RepositoryJdw
+import com.example.ucp2pam.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class DetailJadwalViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryJdw: RepositoryJdw
+) : ViewModel() {
+
+    private val _idAntrian: String = checkNotNull(savedStateHandle[DestinasiDetail.idAntrian])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryJdw.getJdw(_idAntrian)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue =
+            DetailUiState(isLoading = true)
+        )
+
+    fun deleteJdw() {
+        detailUiState.value.detailUiEvent.toJadwalEntity().let {
+            viewModelScope.launch {
+                repositoryJdw.deleteJdw(it)
+            }
+        }
+    }
+}
+

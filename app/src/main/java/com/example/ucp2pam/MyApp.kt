f747fbcdeb052802f395a9db1590b6bebf69a0c4
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-12-24 09:17:44 +0700

    Membuat class MyApp

--- app/src/main/java/com/example/ucp2pam/MyApp.kt
+++ app/src/main/java/com/example/ucp2pam/MyApp.kt
@@ -1,1 +1,14 @@
+package com.example.ucp2pam
 
+import android.app.Application
+import com.example.ucp2pam.dependeciesinjection.ContainerApp
+
+
+class MyApp : Application() {
+    lateinit var containerApp: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        containerApp = ContainerApp(this)
+    }
+}

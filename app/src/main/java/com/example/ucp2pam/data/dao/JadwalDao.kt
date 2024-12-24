65b2205d62f294c9df93e449bf2095f6f752483f
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-12-24 08:33:28 +0700

    membuat JadwalDao

--- app/src/main/java/com/example/ucp2pam/data/dao/JadwalDao.kt
+++ app/src/main/java/com/example/ucp2pam/data/dao/JadwalDao.kt
@@ -1,3 +1,33 @@
 package com.example.ucp2pam.data.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2pam.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface JadwalDao {
+
+    // get all Jadwal
+    @Query("SELECT * FROM jadwal")
+    fun getAllJd(): Flow<List<Jadwal>>
+
+    // get jadwal
+    @Query("SELECT * FROM jadwal WHERE idAntrian = :idAntrian")
+    fun getJd(idAntrian: String): Flow<Jadwal>
+
+    // delete jadwal
+    @Delete
+    suspend fun deleteJd(jadwal: Jadwal)
+
+    // update Jadwal
+    @Update
+    suspend fun updateJd(jadwal: Jadwal)
+
+    @Insert
+    suspend fun insertJd(jadwal: Jadwal)
+}
 

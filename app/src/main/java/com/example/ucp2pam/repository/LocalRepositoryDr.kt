aa129ba62af6d1a0e15d107131a7e8ba3ea806d8
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-12-24 08:39:22 +0700

    membuat Local Repo Dokter

--- app/src/main/java/com/example/ucp2pam/repository/LocalRepositoryDr.kt
+++ app/src/main/java/com/example/ucp2pam/repository/LocalRepositoryDr.kt
@@ -1,3 +1,23 @@
 package com.example.ucp2pam.repository
 
+import com.example.ucp2pam.data.dao.DokterDao
+import com.example.ucp2pam.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryDr(
+    private val dokterDao: DokterDao
+) : RepositoryDr {
+
+    override suspend fun insertDr(dokter: Dokter) {
+        dokterDao.insertDokter(dokter)
+    }
+
+    override fun getAllDr(): Flow<List<Dokter>> {
+        return dokterDao.getAllDokter()
+    }
+
+    override fun getDr(idDr: String): Flow<Dokter> {
+        return dokterDao.getDokter(idDr)
+    }
+}
 

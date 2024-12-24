641b3acd03d372f91a42d23c66cab3660c292d57
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-12-24 08:42:27 +0700

    membuat Repository Jadwal

--- app/src/main/java/com/example/ucp2pam/repository/RepositoryJdw.kt
+++ app/src/main/java/com/example/ucp2pam/repository/RepositoryJdw.kt
@@ -1,2 +1,21 @@
 package com.example.ucp2pam.repository
 
+import com.example.ucp2pam.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryJdw {
+
+    suspend fun insertJdw(jadwal: Jadwal)
+
+    // getAllJdw
+    fun getAllJdw(): Flow<List<Jadwal>>
+
+    // getJdw
+    fun getJdw(idAntrian: String): Flow<Jadwal>
+
+    // deleteJdw
+    suspend fun deleteJdw(jadwal: Jadwal)
+
+    // updateJdw
+    suspend fun updateJdw(jadwal: Jadwal)
+}

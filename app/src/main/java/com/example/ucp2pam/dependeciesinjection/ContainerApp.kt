d4e8a7ab5f26bf80d993cb2fe6d64ffdc3ac9956
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-12-24 08:38:06 +0700

    membuat ContainerApp nya

--- app/src/main/java/com/example/ucp2pam/dependeciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2pam/dependeciesinjection/ContainerApp.kt
@@ -1,2 +1,25 @@
 package com.example.ucp2pam.dependeciesinjection
 
+package com.example.ucp2pam.dependeciesinjection
+
+import android.content.Context
+import com.example.ucp2pam.data.database.KlinikDatabase
+import com.example.ucp2pam.repository.LocalRepositoryDr
+import com.example.ucp2pam.repository.LocalRepositoryJdw
+import com.example.ucp2pam.repository.RepositoryDr
+import com.example.ucp2pam.repository.RepositoryJdw
+
+
+interface InterfaceContainerApp {
+    val repositoryDr: RepositoryDr
+    val repositoryJdw: RepositoryJdw
+}
+
+class ContainerApp(private val context: Context): InterfaceContainerApp {
+    override val repositoryDr: RepositoryDr by lazy {
+        LocalRepositoryDr(KlinikDatabase.getDatabase(context).dokterDao())
+    }
+    override val repositoryJdw: RepositoryJdw by lazy {
+        LocalRepositoryJdw(KlinikDatabase.getDatabase(context).jadwalDao())
+    }
+}
